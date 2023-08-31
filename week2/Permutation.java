c4ffbf5df83c7a34a4127dad8c9d5c92205e903c
Pavels <p.smirnov@outlook.com>
2023-08-31 17:27:28 +0200

    Implement the test cases for permutations

--- week2/Permutation.java
+++ week2/Permutation.java
@@ -5,22 +5,22 @@ import java.util.Iterator;
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
-        if (k > 0) {
+        if (k < 0) {
             throw new IllegalArgumentException("k cannot be smaller than zero");
         }
 
         RandomizedQueue<String> randQueue = new RandomizedQueue<String>();
 
-        while (!StdIn.isEmpty() && k > 0) {
+        while (!StdIn.isEmpty()) {
             String str = StdIn.readString();
             randQueue.enqueue(str);
-            k--;
         }
 
         Iterator<String> iterator = randQueue.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext() && k > 0){
             String str =  iterator.next();
             StdOut.println(str);
+            k--;
         }
     }
 }
