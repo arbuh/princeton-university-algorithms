3ee80745cb7fc77e847a13111c1c54b979adb311
Pavels <p.smirnov@outlook.com>
2023-09-22 18:22:44 +0200

    Add missed dependency and fix formatting

--- week3/FastCollinearPoints.java
+++ week3/FastCollinearPoints.java
@@ -1,153 +1,157 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
+import java.util.Arrays;
 
 public class FastCollinearPoints {
-	private static final double EPSILON = 1E-10;
-	private static final int MIN_POINTS_NR_IN_SEGMENT = 4;
+    private static final double EPSILON = 1E-10;
+    private static final int MIN_POINTS_NR_IN_SEGMENT = 4;
 
-	private int numberOfSegments;
-	private LineSegment[] segments;
+    private int numberOfSegments;
+    private LineSegment[] segments;
 
-	public FastCollinearPoints(Point[] points) {
-		if (points == null) {
-			throw new IllegalArgumentException("argument to BruteCollinearPoints constructor cannot be null");
-		}
+    public FastCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("argument to BruteCollinearPoints constructor cannot be null");
+        }
 
-		for (int i = 0; i < points.length; i++) {
-			Point current = points[i];
+        for (int i = 0; i < points.length; i++) {
+            Point current = points[i];
 
-			if (current == null) {
-				throw new IllegalArgumentException(
-						"argument to BruteCollinearPoints constructor cannot contain a null item");
-			}
+            if (current == null) {
+                throw new IllegalArgumentException(
+                        "argument to BruteCollinearPoints constructor cannot contain a null item");
+            }
 
-			for (int j = i + 1; j < points.length; j++) {
-				if (current.compareTo(points[j]) == 0) {
-					throw new IllegalArgumentException(
-							"argument to BruteCollinearPoints constructor cannot contain a repeating point");
-				}
-			}
-		}
+            for (int j = i + 1; j < points.length; j++) {
+                if (current.compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException(
+                            "argument to BruteCollinearPoints constructor cannot contain a repeating point");
+                }
+            }
+        }
 
-		this.numberOfSegments = 0;
-		this.segments = new LineSegment[0];
-		this.segments = findSegments(points);
-	}
+        this.numberOfSegments = 0;
+        this.segments = new LineSegment[0];
+        findSegments(points);
+    }
 
-	public int numberOfSegments() {
-		return this.numberOfSegments;
-	}
+    public int numberOfSegments() {
+        return this.numberOfSegments;
+    }
 
-	public LineSegment[] segments() {
-		return this.segments;
-	}
+    public LineSegment[] segments() {
+        return this.segments;
+    }
 
-	private LineSegment[] findSegments(Point[] points) {
-		// First, sort points by coordinates
-		Arrays.sort(points);
+    private void findSegments(Point[] points) {
+        // We sort points by coordinates to make possible further identification of
+        // already found segments
+        Arrays.sort(points);
 
-		for (int i = 0; i < points.length; i++) {
-			Point current = points[i];
-			Point[] pointsBySlope = copyArray(points);
-			Arrays.sort(pointsBySlope, current.slopeOrder());
-			findSegment(current, pointsBySlope);
-		}
+        for (int i = 0; i < points.length; i++) {
+            Point current = points[i];
+            Point[] pointsBySlope = copyArray(points);
+            Arrays.sort(pointsBySlope, current.slopeOrder());
+            findSegment(current, pointsBySlope);
+        }
 
-	}
+    }
 
-	// This methods works properly only if `points` are sorted by coordinates
+    // This methods works properly only if `points` are sorted by coordinates
     private void findSegment(Point referencePoint, Point[] points) {
-		int nrOfPointsWithEqualSlope = 1;
+        int nrOfPointsWithEqualSlope = 1;
         boolean isAlreadyFoundSegment = false;
-		double currentSlope = Double.NEGATIVE_INFINITY;
-		Point prevPoint = null;
+        double currentSlope = Double.NEGATIVE_INFINITY;
+        Point prevPoint = null;
 
-		for (int i = 0; i < points.length; i++) {
-			Point point = points[i];
-			double slope = point.slopeTo(referencePoint);
+        for (int i = 0; i < points.length; i++) {
+            Point point = points[i];
+            double slope = point.slopeTo(referencePoint);
 
-			if (areSlopesEqual(slope, currentSlope)) {
-				nrOfPointsWithEqualSlope++;
+            if (areSlopesEqual(slope, currentSlope)) {
+                nrOfPointsWithEqualSlope++;
 
                 // If the point is positioned lower than the reference point,
                 // we assume the segment was already identified in the previous steps,
-                // because we iterate through the points by they positions on the coordinates grid
-				if (referencePoint.compareTo(point) == 1) {
+                // because we iterate through the points by they positions on the coordinates
+                // grid
+                if (referencePoint.compareTo(point) == 1) {
                     isAlreadyFoundSegment = true;
-				}
-			} else {
+                }
+            } else {
 
-                if (nrOfPointsWithEqualSlope >= MIN_POINTS_NR_IN_SEGMENT && !isAlreadyFoundSegment){
-				    this.addSegment(new LineSegment(referencePoint, prevPoint));
+                if (nrOfPointsWithEqualSlope >= MIN_POINTS_NR_IN_SEGMENT && !isAlreadyFoundSegment) {
+                    this.addSegment(new LineSegment(referencePoint, prevPoint));
                 }
 
-				nrOfPointsWithEqualSlope = 1;
+                nrOfPointsWithEqualSlope = 1;
                 isAlreadyFoundSegment = false;
                 currentSlope = slope;
-			}
+            }
 
-			// Add segment if we are in the end of the array
+            // Add segment if we are in the end of the array
             // and the segment minimal length is fulfilled
-			if (i == points.length - 1 && nrOfPointsWithEqualSlope >= MIN_POINTS_NR_IN_SEGMENT && !isAlreadyFoundSegment) {
-				this.addSegment(new LineSegment(referencePoint, prevPoint));
-			}
+            if (i == points.length - 1 && nrOfPointsWithEqualSlope >= MIN_POINTS_NR_IN_SEGMENT
+                    && !isAlreadyFoundSegment) {
+                this.addSegment(new LineSegment(referencePoint, prevPoint));
+            }
 
-			prevPoint = point;
-		}
-	}
+            prevPoint = point;
+        }
+    }
 
-	private static boolean areSlopesEqual(double s0, double s1) {
-		return Math.abs(s0 - s1) < EPSILON;
-	}
+    private static boolean areSlopesEqual(double s0, double s1) {
+        return Math.abs(s0 - s1) < EPSILON;
+    }
 
-	private void addSegment(LineSegment segment) {
-		int newNumber = this.numberOfSegments + 1;
-		LineSegment[] newSegments = new LineSegment[newNumber];
+    private void addSegment(LineSegment segment) {
+        int newNumber = this.numberOfSegments + 1;
+        LineSegment[] newSegments = new LineSegment[newNumber];
 
-		for (int i = 0; i < this.numberOfSegments; i++) {
-			newSegments[i] = this.segments[i];
-		}
-		newSegments[newNumber - 1] = segment;
+        for (int i = 0; i < this.numberOfSegments; i++) {
+            newSegments[i] = this.segments[i];
+        }
+        newSegments[newNumber - 1] = segment;
 
-		this.numberOfSegments = newNumber;
-		this.segments = newSegments;
-	}
+        this.numberOfSegments = newNumber;
+        this.segments = newSegments;
+    }
 
-	private Point[] copyArray(Point[] arr) {
-		Point[] copy = new Point[arr.length];
-		for (int i = 0; i < arr.length; i++)
-			copy[i] = arr[i];
-		return copy;
-	}
+    private Point[] copyArray(Point[] arr) {
+        Point[] copy = new Point[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            copy[i] = arr[i];
+        return copy;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// read the n points from a file
-		In in = new In(args[0]);
-		int n = in.readInt();
-		Point[] points = new Point[n];
-		for (int i = 0; i < n; i++) {
-			int x = in.readInt();
-			int y = in.readInt();
-			points[i] = new Point(x, y);
-		}
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
 
-		// draw the points
-		StdDraw.enableDoubleBuffering();
-		StdDraw.setXscale(0, 32768);
-		StdDraw.setYscale(0, 32768);
-		for (Point p : points) {
-			p.draw();
-		}
-		StdDraw.show();
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
 
-		// print and draw the line segments
-		FastCollinearPoints collinear = new FastCollinearPoints(points);
-		for (LineSegment segment : collinear.segments()) {
-			StdOut.println(segment);
-			segment.draw();
-		}
-		StdDraw.show();
-	}
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
