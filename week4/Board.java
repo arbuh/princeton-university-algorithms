77d88922eb3a9282a12734f7cbb1a9b870c6ae77
Pavels <p.smirnov@outlook.com>
2023-12-20 18:19:08 +0100

    Implement distances

--- week4/Board.java
+++ week4/Board.java
@@ -1,31 +1,34 @@
 public class Board {
     private int[][] tiles;
+    private int[][] goal;
+    private int[][] goalPositions;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        if (tiles.length < 2 || tiles.length <= 128){
+        if (tiles.length < 2 || tiles.length <= 128) {
             throw new IllegalArgumentException("The size of the grid must be between 2 and 128");
         }
 
-        for (int i = 0; i < tiles.length; i++){
-            if (tiles[i].length != tiles.length){
+        for (int i = 0; i < tiles.length; i++) {
+            if (tiles[i].length != tiles.length) {
                 throw new IllegalArgumentException("The grid must be a square");
             }
         }
 
         tiles = copyArray(tiles);
+        createGoal();
     }
-                                           
+
     // string representation of this board
     public String toString() {
         StringBuilder str = new StringBuilder();
 
         str.append(tiles.length);
 
-        for (int i = 0; i < tiles.length; i++){
+        for (int i = 0; i < tiles.length; i++) {
             str.append("\n");
-            for (int j = 0; j < tiles[i].length; j++){
+            for (int j = 0; j < tiles[i].length; j++) {
                 str.append("  " + tiles[i][j]);
             }
         }
@@ -34,18 +37,39 @@ public class Board {
     }
 
     // board dimension n
-    public int dimension(){
+    public int dimension() {
         return tiles.length;
     }
 
     // number of tiles out of place
-    public int hamming(){
-        ???
+    public int hamming() {
+        int distance = 0;
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                if (tiles[i][j] != goal[i][j]) {
+                    distance++;
+                }
+            }
+        }
+
+        return distance;
     }
 
     // sum of Manhattan distances between tiles and goal
-    public int manhattan(){
-        ???
+    public int manhattan() {
+        int distance = 0;
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                int tile = tiles[i][j];
+                int[] goalPosition = goalPositions[tile];
+                int tileDistance = Math.abs(i - goalPosition[0]) + Math.abs(j - goalPosition[1]);
+                distance =+ tileDistance;
+            }
+        }
+
+        return distance;
     }
 
     // is this board the goal board?
@@ -69,7 +93,7 @@ public class Board {
     }
 
     // unit testing (not graded)
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
@@ -87,4 +111,22 @@ public class Board {
         return copy;
     }
 
+    private void createGoal() {
+        int tile = 0;
+        int size = tiles.length * tiles.length;
+
+        for (int i = 0; i < tiles.length; i++) {
+            tile++;
+
+            for (int j = 0; j < tiles[i].length; j++) {
+                // The last tile is the goal position is the empty tile
+                if (tile == size) {
+                    tile = 0;
+                }
+
+                goal[i][j] = tile;
+                goalPositions[tile] = new int[] { i, j };
+            }
+        }
+    }
 }
