aaf2638eff5197e8ad2cc023b87b9cd114a01802
Pavels <p.smirnov@outlook.com>
2023-12-29 19:13:36 +0100

    Implement the solver

--- week4/Solver.java
+++ week4/Solver.java
@@ -1,8 +1,11 @@
 import edu.princeton.cs.algs4.MinPQ;
 import java.util.Comparator;
+import java.util.Stack;
 
 public class Solver {
     private MinPQ<SearchNode> prioQueue;
+    private Stack<Board> solution;
+    private int nrOfMove;
 
     private class SearchNode {
         private Board board;
@@ -23,39 +26,72 @@ public class Solver {
         }
 
         prioQueue = new MinPQ<SearchNode>(new ByHamming());
+        solution = new Stack<SearchNode>();
+        nrOfMove = 0;
+
+        addToQueue(initial, null);
+        solve();
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable()
 
     // min number of moves to solve initial board; -1 if unsolvable
-    public int moves()
+    public int moves() {
+        return nrOfMove;
+    }
 
     // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution()
+    public Iterable<Board> solution() {
+        return solution;
+    }
 
-    // test client (see below) 
+    // test client (see below)
     public static void main(String[] args) {
 
     }
 
+    private void solve(Board initial) {
+        boolean isSolved = false;
+
+        do {
+            SearchNode minPrioNode = prioQueue.min();
+            solution.push(minPrioNode);
+
+            isSolved = minPrioNode.board.isGoal();
+
+            if (!isSolved) {
+                nrOfMove++;
+
+                for (Board neighbor : minPrioNode.board.neighbors()) {
+                    addToQueue(neighbor, minPrioNode);
+                }
+            }
+        } while (!isSolved);
+    }
+
+    private void addToQueue(Board board, SearchNode prev) {
+        SearchNode node = new SearchNode(board, nrOfMove, prev);
+        prioQueue.insert(node);
+    }
+
     private class ByHamming extends Comparator<SearchNode> {
         public int compare(SearchNode node1, SearchNode node2) {
-            double distance1 = node1.board.hamming();
-            double distance2 = node2.board.hamming();
-            compareDistances(distance1, distance2);
+            double prio1 = node1.board.hamming() + node1.nrOfMove;
+            double prio2 = node2.board.hamming() + node2.nrOfMove;
+            comparePriorities(prio1, prio2);
         }
     }
 
     private class ByManhattan extends Comparator<SearchNode> {
         public int compare(SearchNode node1, SearchNode node2) {
-            double distance1 = node1.board.manhattan();
-            double distance2 = node2.board.manhattan();
-            compareDistances(distance1, distance2);
+            double prio1 = node1.board.manhattan() + node1.nrOfMove;
+            double prio2 = node2.board.manhattan() + node2.nrOfMove;
+            comparePriorities(prio1, prio2);
         }
     }
 
-    private int compareDistances(double distance1, double distance2) {
+    private int comparePriorities(double distance1, double distance2) {
         if (distance1 > distance2) {
             return 1;
         } else if (distance1 < distance2) {
