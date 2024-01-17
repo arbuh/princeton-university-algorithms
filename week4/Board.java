3e877fbd1c90e89316c05b838dc6da37345a1e9f
Pavels <p.smirnov@outlook.com>
2024-01-17 20:20:30 +0100

    Reduce memory footprint of Board

--- week4/Board.java
+++ week4/Board.java
@@ -4,8 +4,6 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public final class Board {
     private int[][] tiles;
-    private int[][] positions;
-    private int size;
 
     private Board twin = null;
 
@@ -25,23 +23,12 @@ public final class Board {
             }
         }
 
-        initBoardFromTiles(givenTiles);
+        tiles = copyArray(givenTiles);
     }
 
     // string representation of this board
     public String toString() {
-        StringBuilder str = new StringBuilder();
-
-        str.append(tiles.length);
-
-        for (int i = 0; i < tiles.length; i++) {
-            str.append("\n");
-            for (int j = 0; j < tiles[i].length; j++) {
-                str.append("  " + tiles[i][j]);
-            }
-        }
-
-        return str.toString();
+        return tilesToString(tiles);
     }
 
     // board dimension n
@@ -52,18 +39,7 @@ public final class Board {
     // number of tiles out of place
     public int hamming() {
         if (hamming == -1) {
-            hamming = 0;
-            int tile = 1;
-
-            for (int i = 0; i < tiles.length; i++) {
-                for (int j = 0; j < tiles[i].length; j++) {
-                    if (tiles[i][j] != tile && tile != size) {
-                        hamming++;
-                    }
-
-                    tile++;
-                }
-            }
+            hamming = calcHamming(tiles);
         }
 
         return hamming;
@@ -72,20 +48,7 @@ public final class Board {
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
         if (manhattan == -1) {
-            manhattan = 0;
-            int tile = 0;
-
-            for (int i = 0; i < tiles.length; i++) {
-                for (int j = 0; j < tiles[i].length; j++) {
-                    tile++;
-
-                    if (tile != size) {
-                        int[] position = positions[tile];
-                        int tileDistance = Math.abs(i - position[0]) + Math.abs(j - position[1]);
-                        manhattan += tileDistance;
-                    }
-                }
-            }
+            manhattan = calcManhattan(tiles);
         }
 
         return manhattan;
@@ -93,19 +56,7 @@ public final class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        int tile = 0;
-
-        for (int i = 0; i < tiles.length; i++) {
-            for (int j = 0; j < tiles[i].length; j++) {
-                tile++;
-
-                if (tiles[i][j] != tile && tile != size) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        return hamming() == 0;
     }
 
     // does this board equal y?
@@ -135,63 +86,13 @@ public final class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        int row = positions[0][0];
-        int column = positions[0][1];
-
-        Stack<Board> neighbors = new Stack<Board>();
-
-        int up = row - 1;
-        if (up >= 0) {
-            int[][] copy = copyArray(tiles);
-            copy[row][column] = copy[up][column];
-            copy[up][column] = 0;
-            neighbors.push(new Board(copy));
-        }
-
-        int down = row + 1;
-        if (down < tiles.length) {
-            int[][] copy = copyArray(tiles);
-            copy[row][column] = copy[down][column];
-            copy[down][column] = 0;
-            neighbors.push(new Board(copy));
-        }
-
-        int left = column - 1;
-        if (left >= 0) {
-            int[][] copy = copyArray(tiles);
-            copy[row][column] = copy[row][left];
-            copy[row][left] = 0;
-            neighbors.push(new Board(copy));
-        }
-
-        int right = column + 1;
-        if (right < tiles[row].length) {
-            int[][] copy = copyArray(tiles);
-            copy[row][column] = copy[row][right];
-            copy[row][right] = 0;
-            neighbors.push(new Board(copy));
-        }
-
-        return neighbors;
+        return calcNeighbors(tiles);
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
         if (twin == null) {
-            int tile1 = 0, tile2 = 0, column = 0, row = 0;
-
-            while (tile1 == 0 || tile2 == 0) {
-                row = StdRandom.uniformInt(tiles.length);
-                column = StdRandom.uniformInt(tiles.length - 1);
-                tile1 = tiles[row][column];
-                tile2 = tiles[row][column + 1];
-            }
-
-            int[][] copy = copyArray(tiles);
-            copy[row][column] = tile2;
-            copy[row][column + 1] = tile1;
-
-            twin = new Board(copy);
+            twin = calcTwin(tiles);
         }
 
         return twin;
@@ -226,19 +127,19 @@ public final class Board {
         }
     }
 
-    private void initBoardFromTiles(int[][] arr) {
-        tiles = new int[arr.length][];
-        positions = new int[arr.length * arr.length][];
-        size = arr.length * arr.length;
+    public static String tilesToString(int[][] tiles) {
+        StringBuilder str = new StringBuilder();
 
-        for (int i = 0; i < arr.length; i++) {
-            tiles[i] = arr[i].clone();
+        str.append(tiles.length);
 
-            for (int j = 0; j < arr[i].length; j++) {
-                int tile = tiles[i][j];
-                positions[tile] = new int[] { i, j };
+        for (int i = 0; i < tiles.length; i++) {
+            str.append("\n");
+            for (int j = 0; j < tiles[i].length; j++) {
+                str.append("  " + tiles[i][j]);
             }
         }
+
+        return str.toString();
     }
 
     private static boolean areTilesEqual(int[][] tiles1, int[][] tiles2) {
@@ -258,5 +159,116 @@ public final class Board {
             newArr[i] = arr[i].clone();
         }
         return newArr;
+    }
+
+    private static int calcHamming(int[][] tiles) {
+        int tilesNumber = calcTilesNumber(tiles);
+        int size = tiles.length;
+
+        int hamming = 0;
+
+        for (int i = 0; i < tilesNumber; i++) {
+            int tile = i + 1;
+
+            int raw = i / size;
+            int column = i % size;
+            int actualTile = tiles[raw][column];
+
+            if (actualTile != tile) {
+                hamming++;
+            }
+        }
+
+        return hamming;
+    }
+
+    private static int calcManhattan(int[][] tiles) {
+        int size = tiles.length;
+
+        int manhattan = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int actualTile = tiles[i][j];
+
+                if (actualTile != 0) {
+                    int goalRaw = (actualTile - 1) / size;
+                    int goalColumn = (actualTile - 1) % size;
+                    int distance = Math.abs(i - goalRaw) + Math.abs(j - goalColumn);
+                    manhattan += distance;
+                }
+            }
+        }
+
+        return manhattan;
+    }
+
+    private static int calcTilesNumber(int[][] tiles) {
+        return tiles.length * tiles.length - 1;
+    }
+
+    private static Iterable<Board> calcNeighbors(int[][] tiles) {
+        int blankRaw = 0, blankColumn = 0;
+
+        outerloop: for (blankRaw = 0; blankRaw < tiles.length; blankRaw++) {
+            for (blankColumn = 0; blankColumn < tiles.length; blankColumn++) {
+                if (tiles[blankRaw][blankColumn] == 0) {
+                    break outerloop;
+                }
+            }
+        }
+
+        Stack<Board> neighbors = new Stack<Board>();
+
+        int up = blankRaw - 1;
+        if (up >= 0) {
+            int[][] copy = copyArray(tiles);
+            copy[blankRaw][blankColumn] = copy[up][blankColumn];
+            copy[up][blankColumn] = 0;
+            neighbors.push(new Board(copy));
+        }
+
+        int down = blankRaw + 1;
+        if (down < tiles.length) {
+            int[][] copy = copyArray(tiles);
+            copy[blankRaw][blankColumn] = copy[down][blankColumn];
+            copy[down][blankColumn] = 0;
+            neighbors.push(new Board(copy));
+        }
+
+        int left = blankColumn - 1;
+        if (left >= 0) {
+            int[][] copy = copyArray(tiles);
+            copy[blankRaw][blankColumn] = copy[blankRaw][left];
+            copy[blankRaw][left] = 0;
+            neighbors.push(new Board(copy));
+        }
+
+        int right = blankColumn + 1;
+        if (right < tiles[blankRaw].length) {
+            int[][] copy = copyArray(tiles);
+            copy[blankRaw][blankColumn] = copy[blankRaw][right];
+            copy[blankRaw][right] = 0;
+            neighbors.push(new Board(copy));
+        }
+
+        return neighbors;
+    }
+
+    private static Board calcTwin(int[][] tiles) {
+        int tile1 = 0, tile2 = 0, column = 0, row = 0;
+
+        while (tile1 == 0 || tile2 == 0) {
+            row = StdRandom.uniformInt(tiles.length);
+            column = StdRandom.uniformInt(tiles.length - 1);
+            tile1 = tiles[row][column];
+            tile2 = tiles[row][column + 1];
+        }
+
+        int[][] copy = copyArray(tiles);
+        copy[row][column] = tile2;
+        copy[row][column + 1] = tile1;
+
+        return new Board(copy);
     }
 }
