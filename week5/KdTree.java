2b0a5ac966b8322fdb54a59c78bf1a7d144d1943
pavels <p.smirnov@outlook.com>
2026-02-06 11:07:54 +0100

    Implement KdTree range serach

--- week5/KdTree.java
+++ week5/KdTree.java
@@ -2,6 +2,8 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.ArrayList;
+
 public class KdTree {
     private Node root;
     private int size;
@@ -46,7 +48,7 @@ public class KdTree {
             this.root = new Node(point, Alignment.VERTICAL);
         }
 
-        insertRec(point, root);
+        insertRec(point, this.root);
         this.size++;
     }
 
@@ -83,13 +85,12 @@ public class KdTree {
         }
     }
 
-    // TODO
     public boolean contains(Point2D point) {
         if (point == null) {
             throw new IllegalArgumentException();
         }
 
-        return false;
+        return containsRec(point, this.root);
     }
 
     private boolean containsRec(Point2D point, Node node) {
@@ -120,7 +121,7 @@ public class KdTree {
     }
 
     public void draw() {
-        drawRec(root);
+        drawRec(this.root);
     }
 
     private void drawRec(Node node) {
@@ -133,12 +134,52 @@ public class KdTree {
         drawRec(node.right);
     }
 
-    // TODO
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) {
             throw new IllegalArgumentException();
         }
-        return null;
+        return rangeRec(rect, this.root);
+    }
+
+    private ArrayList<Point2D> rangeRec(RectHV rect, Node node) {
+        ArrayList<Point2D> list = new ArrayList<>();
+
+        if (node == null) {
+            return list;
+        }
+
+        if (rect.contains(node.value)) {
+            list.add(node.value);
+        }
+
+        double rectMin, rectMax, nodeCoord;
+        if (node.alignment == Alignment.VERTICAL) {
+            rectMin = rect.xmin();
+            rectMax = rect.xmax();
+            nodeCoord = node.value.x();
+        }
+        else {
+            rectMin = rect.ymin();
+            rectMax = rect.ymax();
+            nodeCoord = node.value.y();
+        }
+
+        if (rectMin < nodeCoord && rectMax > nodeCoord) {
+            ArrayList<Point2D> left = rangeRec(rect, node.left);
+            list.addAll(left);
+            ArrayList<Point2D> right = rangeRec(rect, node.right);
+            list.addAll(right);
+        }
+        else if (rectMax < nodeCoord) {
+            ArrayList<Point2D> left = rangeRec(rect, node.left);
+            list.addAll(left);
+        }
+        else {
+            ArrayList<Point2D> right = rangeRec(rect, node.right);
+            list.addAll(right);
+        }
+
+        return list;
     }
 
     // TODO
